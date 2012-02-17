acc914310da75324296a310c31b1148285b571ea
robertosf90 <robertosf90@gmail.com>
2012-02-17 17:53:25 +0000

    [NA]: Criação das classes básicas

---
+++ src/main/java/com/hivelog/pamcard/cfe/imp/CfeConfigImp.java
@@ -0,0 +1,7 @@
+package com.hivelog.pamcard.cfe.imp;
+
+import com.hivelog.pamcard.cfe.CfeConfig;
+
+public class CfeConfigImp implements CfeConfig {
+
+}
---
+++ src/main/java/com/hivelog/pamcard/cfe/imp/CfeFacadeImp.java
@@ -0,0 +1,7 @@
+package com.hivelog.pamcard.cfe.imp;
+
+import com.hivelog.pamcard.cfe.CfeFacade;
+
+public class CfeFacadeImp implements CfeFacade {
+
+}
