1d9f26d76be76881ec0e78bddce265a8c7a60ef8
robertosf90 <robertosf90@gmail.com>
2012-02-17 18:07:45 +0000

    [NA]: Criação das classes básicas

---
+++ src/test/java/com/hivelog/pamcard/cfe/Teste.java
@@ -0,0 +1,29 @@
+package com.hivelog.pamcard.cfe;
+
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
+import br.com.hst.jee.core.to.webservice.FieldTO;
+import br.com.hst.jee.core.to.webservice.RequestTO;
+import br.com.hst.jee.core.to.webservice.ResponseTO;
+import br.com.pamcary.jee.pamcard.webservice.WSPamcardServiceLocator;
+
+public class Teste {
+	
+	public static void main (String [] args) throws RemoteException, ServiceException{
+		RequestTO request = new RequestTO();
+		request.setCertificate("");
+		request.setContext("FindCard");
+		FieldTO field = new FieldTO();
+		field.setKey("viagem.cartao.status.id");
+		field.setValue("1");
+		request.setFields(new FieldTO[] { field });
+		WSPamcardServiceLocator locator = new WSPamcardServiceLocator();
+		ResponseTO response = locator.getWSPamcard().execute(request);
+		System.out.println(response.getContext());
+		System.out.println(response.getCertificate());
+		System.out.println(response.getToken());
+	}
+
+}
